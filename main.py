cab03ddcdb138b5ebf9514358f9d617dd2416776
ForkIRL <finnsesterhenn282@gmail.com>
2025-03-23 19:00:06 +0100

    Displaying wordcount

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
+def word_count(book_text):
+    amount = len(book_text.split())
+    return amount
 
-print(get_book_text("books/frankenstein.txt"))
+
+num_words = word_count(get_book_text("books/frankenstein.txt"))
+print(f"{num_words} words found in the document")
+
+
+
+
+
+
 
