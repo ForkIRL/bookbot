fe035e8e02e8029a7556334f2814d62acd720184
ForkIRL <finnsesterhenn282@gmail.com>
2025-03-28 11:48:01 +0100

    Updated main.py to store counting function in variable and print it

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from stats import get_book_text
 from stats import word_count
 from stats import character_count
 
+character_counter=character_count(get_book_text("books/frankenstein.txt"))
+print(character_counter)
 
-print(character_count(get_book_text("books/frankenstein.txt")))
 
 
 
