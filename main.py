6cdb378b4d7e800a0c2b110763dd04578190aca2
ForkIRL <finnsesterhenn282@gmail.com>
2025-03-28 19:14:17 +0100

    Finished up the report

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from stats import get_book_text
-from stats import word_count
+from stats import word_counter
 from stats import character_count
+from stats import sort_on
+file_path = "books/frankenstein.txt"
+book_text = get_book_text(file_path)
+word_count = word_counter(book_text)
 
-character_counter=character_count(get_book_text("books/frankenstein.txt"))
-print(character_counter)
 
 
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at{file_path}...")
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")
+print("--------- Character Count -------")
+new = sort_on(character_count(book_text))
+for entry in new:
+    print(f"{entry["char"]}: {entry["count"]}")
+print("============= END ===============")
 
 
 
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
-def word_count(book_text):
+def word_counter(book_text):
     amount = len(book_text.split())
     return amount
 def character_count(book_text):
@@ -14,3 +14,13 @@ def character_count(book_text):
         else:
             character_dic[character] = 1
     return character_dic
+def sort_value(dict):
+    for character in dict:
+        return dict[character]
+def sort_on(characters):
+    new_list = []
+    for character, count in characters.items():
+        if character.isalpha():
+            new_list.append({"char": character, "count": count})
+            new_list.sort(reverse=True, key=lambda x: x["count"])
+    return(new_list)
