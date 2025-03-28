b6dbaad8b2103fa7adc43ee8a79a0ea4087199f7
ForkIRL <finnsesterhenn282@gmail.com>
2025-03-28 19:29:46 +0100

    Added the path to the input of program

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from stats import get_book_text
-from stats import word_counter
-from stats import character_count
-from stats import sort_on
-file_path = "books/frankenstein.txt"
-book_text = get_book_text(file_path)
-word_count = word_counter(book_text)
+from stats import get_book_text, word_counter, character_count, sort_on
+import sys
+def main():
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        file_path = sys.argv[1]
+        book_text = get_book_text(file_path)
+        word_count = word_counter(book_text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at{file_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    new = sort_on(character_count(book_text))
+    for entry in new:
+        print(f"{entry["char"]}: {entry["count"]}")
+    print("============= END ===============")
 
 
-
-print("============ BOOKBOT ============")
-print(f"Analyzing book found at{file_path}...")
-print("----------- Word Count ----------")
-print(f"Found {word_count} total words")
-print("--------- Character Count -------")
-new = sort_on(character_count(book_text))
-for entry in new:
-    print(f"{entry["char"]}: {entry["count"]}")
-print("============= END ===============")
-
-
-
+main()
 
 
 
